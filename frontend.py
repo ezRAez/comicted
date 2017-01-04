f1ca2ec51bcf4de6cf68eb188aac71f170254243
Ezra Raez <ezRAez10@gmail.com>
2017-01-04 12:33:15 -0800

    add update method

--- frontend.py
+++ frontend.py
@@ -53,6 +53,19 @@ e4.grid(row = 1, column = 3)
 
 # List and Scrollbar
 
+def get_selected_row(event):
+    global sel_tuple
+    index = list1.curselection()[0]
+    sel_tuple = list1.get(index)
+    e1.delete(0, END)
+    e1.insert(END, sel_tuple[1])
+    e2.delete(0, END)
+    e2.insert(END, sel_tuple[2])
+    e3.delete(0, END)
+    e3.insert(END, sel_tuple[3])
+    e4.delete(0, END)
+    e4.insert(END, sel_tuple[4])
+
 list1 = Listbox(window, height = 6, width = 35)
 list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 2)
 
@@ -61,6 +74,8 @@ sb1.grid(row = 2, column = 2, rowspan = 6)
 
 list1.configure(yscrollcommand = sb1.set)
 sb1.configure(command = list1.yview)
+
+list1.bind('<<ListboxSelect>>', get_selected_row)
 
 # Commands
 
@@ -79,6 +94,14 @@ def insert_command():
     list1.delete(0, END)
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
 
+def delete_command():
+    backend.delete(sel_tuple[0])
+    view_command()
+
+def update_command():
+    backend.update(sel_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    view_command()
+
 # Buttons
 
 b1 = Button(window, text = "View All", width = 12, command = view_command)
@@ -90,10 +113,10 @@ b2.grid(row = 3, column = 3)
 b3 = Button(window, text = "Add Entry", width = 12, command = insert_command)
 b3.grid(row = 4, column = 3)
 
-b4 = Button(window, text = "Update Selected", width = 12)
+b4 = Button(window, text = "Update Selected", width = 12, command = update_command)
 b4.grid(row = 5, column = 3)
 
-b5 = Button(window, text = "Delete Selected", width = 12)
+b5 = Button(window, text = "Delete Selected", width = 12, command = delete_command)
 b5.grid(row = 6, column = 3)
 
 b6 = Button(window, text = "Close", width = 12)
