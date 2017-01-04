add850a4e10acdfdd33351a2e18192d1ab0f7035
Ezra Raez <ezRAez10@gmail.com>
2017-01-04 13:53:59 -0800

    refactor backend

--- backend.py
+++ backend.py
@@ -3,45 +3,32 @@ import sqlite3
 class Database:
 
     def __init__(self, db):
-        conn = sqlite3.connect(db)
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS comic (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn text)")
-        conn.commit()
-        conn.close()
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS comic (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn text)")
+        self.conn.commit()
 
     def insert(self, title, author, year, isbn):
-        conn = sqlite3.connect("comicted.db")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO comic VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
-        conn.commit()
-        conn.close()
+        self.cur.execute("INSERT INTO comic VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+        self.conn.commit()
 
     def view(self):
-        conn = sqlite3.connect("comicted.db")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM comic")
-        rows = cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM comic")
+        rows = self.cur.fetchall()
         return rows
 
     def search(self, title = "", author = "", year = "", isbn = ""):
-        conn = sqlite3.connect("comicted.db")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM comic WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
-        rows = cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM comic WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+        rows = self.cur.fetchall()
         return rows
 
     def delete(self, id):
-        conn = sqlite3.connect("comicted.db")
-        cur = conn.cursor()
-        cur.execute("DELETE FROM comic WHERE id=?", (id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("DELETE FROM comic WHERE id=?", (id,))
+        self.conn.commit()
 
     def update(self, id, title, author, year, isbn):
-        conn = sqlite3.connect("comicted.db")
-        cur = conn.cursor()
-        cur.execute("UPDATE comic SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE comic SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
