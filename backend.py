093999bb1a1985bec10eb7fbeac511b86cbf37da
Ezra Raez <ezRAez10@gmail.com>
2017-01-04 01:01:52 -0800

    separate front and backend

---
+++ backend.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect("comicted.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS comic (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn text)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("comicted.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO comic VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("comicted.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM comic")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
